e21c6b5c2e74e28ba09299a8d1d49ffa7b30b51d
Allan Kalnay <a.kalnay@sportradar.com>
2020-10-31 13:09:56 +0100

    schedule update, run update

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 
 # Add source roots to PYTHONPATH
-export PYTHONPATH=`pwd`
+
 
 cd ~/development/trnava-covid19-testing-scraper/src/main/python
+echo `pwd`
+export PYTHONPATH=`pwd`
 
 source ~/programs/anaconda3/etc/profile.d/conda.sh
 conda activate trnava-covid19-testing-scraper
