5fd7b73ebeeefedcfb5fe5ddaaee5e628b99e982
Allan Kalnay <a.kalnay@sportradar.com>
2020-10-31 12:37:37 +0100

    fixed run script

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 # Add source roots to PYTHONPATH
 export PYTHONPATH=`pwd`
 
-cd src/main/python
+cd ~/development/trnava-covid19-testing-scraper/src/main/python
+
+source ~/programs/anaconda3/etc/profile.d/conda.sh
+conda activate trnava-covid19-testing-scraper
 python3 ./main.py
